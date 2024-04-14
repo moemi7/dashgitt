e901dc5bb05642bbc894052d41d8c0269bc5b034
Mohamed Taouil <moemi7@hotmail.com>
2024-04-14 13:40:16 +0200

    twee

--- src/pages/offertes/index.tsx
+++ src/pages/offertes/index.tsx
@@ -1,41 +1,41 @@
-import { useRouter } from '@/routes/hooks';
-import { Button } from '@/components/ui/button';
-import  Empty from './components/empty';
+import PageHead from '@/components/shared/page-head';
+import { useGetStudents } from './queries/queries';
+import StudentsTable from './components/students-table';
+import { useSearchParams } from 'react-router-dom';
+import { DataTableSkeleton } from '@/components/shared/data-table-skeleton';
 
+export default function StudentPage() {
+  const [searchParams] = useSearchParams();
+  const page = Number(searchParams.get('page') || 1);
+  const pageLimit = Number(searchParams.get('limit') || 10);
+  const country = searchParams.get('search') || null;
+  const offset = (page - 1) * pageLimit;
+  const { data, isLoading } = useGetStudents(offset, pageLimit, country);
+  const users = data?.users;
+  const totalUsers = data?.total_users; //1000
+  const pageCount = Math.ceil(totalUsers / pageLimit);
 
-export default function NotFound() {
-  const router = useRouter();
+  if (isLoading) {
+    return (
+      <div className="p-5">
+        <DataTableSkeleton
+          columnCount={10}
+          filterableColumnCount={2}
+          searchableColumnCount={1}
+        />
+      </div>
+    );
+  }
 
   return (
-    <div className="absolute left-1/2 top-1/2 mb-16 -translate-x-1/2 -translate-y-1/2 items-center justify-center text-center">
-      
-      <span className="bg-gradient-to-b from-foreground to-transparent bg-clip-text text-[10rem] font-extrabold leading-none text-transparent">
-        404
-      </span>
-      <h2 className="font-heading my-2 text-2xl font-bold">
-        Something&apos;s missing
-      </h2>
-      <p>
-        Sorry, the page you are looking for doesn&apos;t exist or has been
-        moved.
-      </p>
-      <div>
-      <div>
-        <Empty/>
-
-
-
-      </div>
-      
-      <div className="mt-8 flex justify-center gap-2">
-        <Button onClick={() => router.back()} variant="default" size="lg">
-          Go back
-        </Button>
-        <Button onClick={() => router.push('/')} variant="ghost" size="lg">
-          Back to Home
-        </Button>
-      </div>
-      </div>
+    <div className="p-5">
+      <PageHead title="Student Management | App" />
+      <StudentsTable
+        users={users}
+        page={page}
+        totalUsers={totalUsers}
+        pageCount={pageCount}
+      />
     </div>
   );
 }
