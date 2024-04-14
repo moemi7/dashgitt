bf38821b51c593257bd42467f8524f06a83608f2
Mohamed Taouil <moemi7@hotmail.com>
2024-04-14 10:08:07 +0200

    Update landingspageinput.tsx

--- src/pages/configurator/components/landingspageinput.tsx
+++ src/pages/configurator/components/landingspageinput.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 
-import { Button, buttonVariants } from '@/components/ui/button';
+import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { useRouter } from '@/routes/hooks';
 
